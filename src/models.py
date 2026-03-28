d4ad79ae8e189e7f9b45a26e5e396962abe2e939
Ignacio Pinto <51886210+Pinto2291@users.noreply.github.com>
2026-03-28 04:09:31 +0000

    primera tabla creada y clase User modificado, tabla o clase Post creada falta conectar la foreignKey

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
@@ -9,6 +9,7 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
 
 
     def serialize(self):
@@ -17,3 +18,10 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+     
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    image_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    description: Mapped[str] = mapped_column(String(500)) # Pudiera estar vacio
+    user_id: Mapped[int] = mapped_column(ForeignKey('user_id'))
