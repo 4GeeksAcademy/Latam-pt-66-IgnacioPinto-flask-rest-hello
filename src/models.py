ed6ef9991149b208e33d567284a17bf401f3dc85
Ignacio Pinto <51886210+Pinto2291@users.noreply.github.com>
2026-03-30 16:02:58 +0000

    proyecto finalizado

--- src/models.py
+++ src/models.py
@@ -19,9 +19,22 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
-     
+#Tabla de Posts, cada post tiene una imagen y una descripcion, ademas de un usuario que lo creo
 class Post(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     image_url: Mapped[str] = mapped_column(String(250), nullable=False)
     description: Mapped[str] = mapped_column(String(500)) # Pudiera estar vacio
-    user_id: Mapped[int] = mapped_column(ForeignKey('user_id'))
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+
+#tabla de comment cada comentarios tiene un user ID y un POST ID cada comentarios pertenese a un usuario y cada comentarios pertenese a un post
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    content: Mapped[str] = mapped_column(String(500), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
+
+
+class Follower(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
